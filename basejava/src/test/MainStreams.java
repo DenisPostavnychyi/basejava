06b996b3a504cca76f81e870940ffa5b11d3701c
DenisPostavnychyi <denozaurus03@ukr.net>
2022-08-03 11:15:55 +0300

    Twelfth lesson homework

---
+++ basejava/src/test/MainStreams.java
@@ -0,0 +1,58 @@
+package test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStreams {
+    public static void main(String[] args) {
+        /*Scanner in = new Scanner(System.in);
+        System.out.println("Enter array length");
+        int[] numbers = new int[in.nextInt()];
+        System.out.println("Enter numbers");
+        String strNums = in.nextLine();
+        String[] nums = strNums.trim().split(" ");
+        for (int i = 0; i < numbers.length; i++)
+        {
+            numbers[i] = Integer.parseInt(nums[i]);
+            System.out.println(numbers[i]);
+        }*/
+
+        int[] values1 = new int[]{3, 3, 2, 3, 1, 2};
+        int[] values2 = new int[]{9, 8};
+
+        List<Integer> integers1 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> integers2 = Arrays.asList(2, 3, 4, 5, 6);
+
+        System.out.println(minValue(values1));
+        System.out.println(minValue(values2));
+
+        System.out.println("Print list with odd sum");
+        printList(oddOrEven(integers1));
+
+        System.out.println("Print list with even sum");
+        printList(oddOrEven(integers2));
+    }
+
+    private static int minValue(int[] values) {
+        int num = Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce((acc, x) -> (acc * 10) + x)
+                .getAsInt();
+        return num;
+    }
+
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream()
+                .filter(integers.stream().mapToInt(Integer::intValue)
+                        .sum() % 2 != 0 ? n -> n % 2 == 0 : n -> n % 2 != 0)
+                .collect(Collectors.toList());
+    }
+
+    private static void printList(List<Integer> integers) {
+        for (int num : integers) {
+            System.out.println(num);
+        }
+    }
+}

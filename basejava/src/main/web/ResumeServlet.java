5a60847ad8643a25afabe837f0840dfe4df558e6
DenisPostavnychyi <denozaurus03@ukr.net>
2022-08-12 14:52:03 +0300

    Fifteenth lesson hommework update

--- basejava/src/main/web/ResumeServlet.java
+++ basejava/src/main/web/ResumeServlet.java
@@ -1,53 +1,65 @@
 package main.web;
 
+import main.Config;
+import main.model.Resume;
+import main.storage.Storage;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-
-import static test.TestData.*;
+import java.io.Writer;
 
 @WebServlet("/resume")
 public class ResumeServlet extends HttpServlet {
+
+    private Storage storage; //= Config.get().getStorage();
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        /*request.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');*/
 
 
-        PrintWriter pw = response.getWriter();
-        pw.println("<html>");
-
-        pw.println("<style>table, th, td {border:1px solid black;}</style>");
-
-        pw.println("<h2>Resume Table</h2>");
-
-        pw.println("<table style=\"width:50%\">");
-        pw.println("<tr>");
-        pw.println("<th>" + R1.getUuid() + "</th>");
-        pw.println("<th>" + R1.getFullName() + "</th>");
-        pw.println("</tr>");
-
-        pw.println("<tr>");
-        pw.println("<th>" + R2.getUuid() + "</th>");
-        pw.println("<th>" + R2.getFullName() + "</th>");
-        pw.println("</tr>");
-
-        pw.println("<tr>");
-        pw.println("<th>" + R3.getUuid() + "</th>");
-        pw.println("<th>" + R3.getFullName() + "</th>");
-        pw.println("</tr>");
-        pw.println("</table>");
-
-        pw.println("</html>");
+        Writer writer = response.getWriter();
+        writer.write(
+                "<html>\n" +
+                        "<head>\n" +
+                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
+                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
+                        "    <title>Список всех резюме</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "<section>\n" +
+                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
+                        "    <tr>\n" +
+                        "        <th>Uuid</th>\n" +
+                        "        <th>Ім'я</th>\n" +
+                        "    </tr>\n");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>\n" +
+                            "     <td>" + resume.getUuid() + "</td>\n" +
+                            "     <td>" + resume.getFullName() + "</td>\n" +
+                            "</tr>\n");
+        }
+        writer.write("</table>\n" +
+                "</section>\n" +
+                "</body>\n" +
+                "</html>\n");
     }
 }
